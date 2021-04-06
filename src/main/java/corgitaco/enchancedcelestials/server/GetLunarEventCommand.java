acca95a5d4b3f48df790af3df77e890b0e9e2022
jellybean13 <jellybean13@jellybean.org>
2021-04-06 21:32:26 +0800

    Optimize "/lunarevent get" command.

--- src/main/java/corgitaco/enchancedcelestials/server/GetLunarEventCommand.java
+++ src/main/java/corgitaco/enchancedcelestials/server/GetLunarEventCommand.java
@@ -16,9 +16,13 @@ public class GetLunarEventCommand {
 
     public static int getLunarEventType(CommandSource source) {
         long dayTime = EnhancedCelestialsUtils.modulosDaytime(source.getWorld().getWorld().getWorldInfo().getDayTime());
-        source.sendFeedback((dayTime >= 12000 && dayTime <= 24000) ? new TranslationTextComponent("enhancedcelestials.commands.get.success", new TranslationTextComponent(EnhancedCelestials.currentLunarEvent.getName()))
-                                                                   : new TranslationTextComponent("enhancedcelestials.commands.get.success.no_event"),
-                                                                   true);
+        if (dayTime >= 12000 && dayTime <= 24000) {
+            source.sendFeedback(new TranslationTextComponent("enhancedcelestials.commands.get.success", new TranslationTextComponent(EnhancedCelestials.currentLunarEvent.getName())), true);
+        } else if (EnhancedCelestials.nextNightLunarEvent != null) {
+            source.sendFeedback(new TranslationTextComponent("enhancedcelestials.commands.get.success.next_night", new TranslationTextComponent(EnhancedCelestials.nextNightLunarEvent.getName())), true);
+        } else {
+            source.sendFeedback(new TranslationTextComponent("enhancedcelestials.commands.get.success.no_event"), true);
+        }
         return 1;
     }
 }
