433e0666cabdf357c5cf3036b838e5df0d3c6024
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-08-17 12:12:17 -0700

    -Apply moon texture from lunar event

--- src/main/java/corgitaco/enhancedcelestials/mixin/client/MixinWorldRenderer.java
+++ src/main/java/corgitaco/enhancedcelestials/mixin/client/MixinWorldRenderer.java
@@ -5,14 +5,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import corgitaco.enhancedcelestials.EnhancedCelestialsWorldData;
 import corgitaco.enhancedcelestials.LunarContext;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
@@ -20,12 +20,24 @@ public abstract class MixinWorldRenderer {
     @Shadow
     private ClientWorld world;
 
+    @Shadow @Final private static ResourceLocation MOON_PHASES_TEXTURES;
+
     @Inject(method = "renderSky(Lcom/mojang/blaze3d/matrix/MatrixStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getMoonPhase()I"))
     private void changeMoonColor(MatrixStack matrixStackIn, float partialTicks, CallbackInfo ci) {
         LunarContext lunarContext = ((EnhancedCelestialsWorldData) this.world).getLunarContext();
         if (lunarContext != null) {
             Vector3f glColor = lunarContext.getCurrentEvent().getClientSettings().getColorSettings().getGLMoonColor();
             RenderSystem.color4f(glColor.getX(), glColor.getY(), glColor.getZ(), 1.0F - this.world.getRainStrength(partialTicks));
+        }
+    }
+
+    @Redirect(method = "renderSky(Lcom/mojang/blaze3d/matrix/MatrixStack;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V", ordinal = 1))
+    private void bindCustomMoonTexture(TextureManager textureManager, ResourceLocation resource) {
+        LunarContext lunarContext = ((EnhancedCelestialsWorldData) this.world).getLunarContext();
+        if (lunarContext != null) {
+            textureManager.bindTexture(lunarContext.getCurrentEvent().getClient().getMoonTextureLocation());
+        } else {
+            textureManager.bindTexture(MOON_PHASES_TEXTURES);
         }
     }
 
