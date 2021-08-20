f14c3efb90a65b39229c441efae9abf60455746d
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-08-19 22:55:36 -0700

    -Really bad loot table appending aheehee

---
+++ src/main/java/corgitaco/enhancedcelestials/mixin/MixinLootTableManager.java
@@ -0,0 +1,53 @@
+package corgitaco.enhancedcelestials.mixin;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import corgitaco.enhancedcelestials.mixin.access.JsonReloadListenerAccess;
+import net.minecraft.client.resources.JsonReloadListener;
+import net.minecraft.loot.LootTableManager;
+import net.minecraft.resources.IResource;
+import net.minecraft.resources.IResourceManager;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+@Mixin(LootTableManager.class)
+public abstract class MixinLootTableManager extends JsonReloadListener {
+
+    public MixinLootTableManager(Gson gson, String string) {
+        super(gson, string);
+    }
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$apply$0(Lnet/minecraft/resources/IResourceManager;Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/util/ResourceLocation;Lcom/google/gson/JsonElement;)V", at = @At("HEAD"), remap = false)
+    private void appendTables(IResourceManager resourceManagerIn, ImmutableMap.Builder builder, ResourceLocation location, JsonElement element, CallbackInfo ci) {
+        appendTable(resourceManagerIn, location, element);
+    }
+
+    private void appendTable(IResourceManager resourceManager, ResourceLocation id, JsonElement element) {
+        try (IResource appendedTable = resourceManager.getResource(getAppendedPath(id))) {
+            InputStream inputstream = appendedTable.getInputStream();
+
+            Reader reader = new BufferedReader(new InputStreamReader(inputstream, StandardCharsets.UTF_8));
+            JsonElement appendedElement = JSONUtils.fromJson(((JsonReloadListenerAccess) this).getGson(), reader, JsonElement.class);
+
+            JsonArray pools = element.getAsJsonObject().getAsJsonArray("pools");
+
+            pools.addAll(appendedElement.getAsJsonObject().getAsJsonArray("pools"));
+        } catch (IOException e) {
+
+        }
+    }
+
+    private static ResourceLocation getAppendedPath(ResourceLocation rl) {
+        return new ResourceLocation(rl.getNamespace(), "appendedloottables/" + rl.getPath() + ".json");
+    }
+}
