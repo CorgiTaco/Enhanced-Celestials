fb18db6e779d19e19038d948b223928da0c77dc3
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-08-24 23:04:26 -0700

    Appending loot tables is now unnoticeable and optimized, bump version

--- src/main/java/corgitaco/enhancedcelestials/mixin/MixinLootTableManager.java
+++ src/main/java/corgitaco/enhancedcelestials/mixin/MixinLootTableManager.java
@@ -1,10 +1,9 @@
 package corgitaco.enhancedcelestials.mixin;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import corgitaco.enhancedcelestials.Main;
 import corgitaco.enhancedcelestials.mixin.access.JsonReloadListenerAccess;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
@@ -13,59 +12,45 @@ import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.storage.loot.LootTables;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.Map;
 
 @Mixin(LootTables.class)
 public abstract class MixinLootTableManager extends SimpleJsonResourceReloadListener {
 
-    @Shadow @Final private static Gson GSON;
-
     public MixinLootTableManager(Gson gson, String string) {
         super(gson, string);
     }
 
-    private static final ThreadLocal<ResourceManager> RESOURCE_MANAGER = new ThreadLocal<>();
-
-    @Inject(method = "apply", at = @At(value = "HEAD"))
-    private void attachResourceManager(Map<ResourceLocation, JsonElement> map, ResourceManager resourceManager, ProfilerFiller profilerFiller, CallbackInfo ci) {
-        RESOURCE_MANAGER.set(resourceManager);
-    }
-
-    @Inject(method = "apply", at = @At("RETURN"))
-    private void detachResourceManager(Map<ResourceLocation, JsonElement> map, ResourceManager resourceManager, ProfilerFiller profilerFiller, CallbackInfo ci) {
-        RESOURCE_MANAGER.set(null);
-    }
-
-    @SuppressWarnings("UnresolvedMixinReference")
-    @Inject(method = "lambda$apply$0(Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonElement;)V", at = @At("HEAD"))
-    private static void appendTables(ImmutableMap.Builder builder, ResourceLocation location, JsonElement element, CallbackInfo ci) {
-        appendTable(RESOURCE_MANAGER.get(), location, element);
-    }
-
-    private static void appendTable(ResourceManager resourceManager, ResourceLocation id, JsonElement element) {
-        for (String resourceNamespace : resourceManager.getNamespaces()) {
-            try (Resource appendedTable = resourceManager.getResource(getAppendedPath(resourceNamespace, id))) {
-                InputStream inputstream = appendedTable.getInputStream();
-                Reader reader = new BufferedReader(new InputStreamReader(inputstream, StandardCharsets.UTF_8));
-                JsonElement appendedElement = GsonHelper.fromJson(GSON, reader, JsonElement.class);
-                JsonArray pools = element.getAsJsonObject().getAsJsonArray("pools");
-                pools.addAll(appendedElement.getAsJsonObject().getAsJsonArray("pools"));
-            } catch (IOException e) {
-
+    @Inject(method = "apply", at = @At("HEAD"))
+    private void appendTables(Map<ResourceLocation, JsonElement> values, ResourceManager resourceManager, ProfilerFiller profilerFiller, CallbackInfo ci) {
+        String appendTablesDir = "append_loot_tables";
+        Collection<ResourceLocation> resourceLocations = resourceManager.listResources(appendTablesDir, (key) -> key.endsWith(".json"));
+        for (ResourceLocation resourceLocation : resourceLocations) {
+            ResourceLocation key = new ResourceLocation(resourceLocation.getPath().replace(appendTablesDir + "/", "").replaceFirst("/", ":").replace(".json", ""));
+            if (values.containsKey(key)) {
+                values.get(key).getAsJsonObject().getAsJsonArray("pools").addAll(extractPools(resourceManager, resourceLocation));
             }
         }
     }
 
-    private static ResourceLocation getAppendedPath(String s, ResourceLocation resourceLocation) {
-        return new ResourceLocation(s, "append_loot_tables/" + resourceLocation.toString().replace(":", "/") + ".json");
+    private JsonArray extractPools(ResourceManager resourceManager, ResourceLocation location) {
+        try (Resource appendedTable = resourceManager.getResource(location)) {
+            InputStream inputstream = appendedTable.getInputStream();
+            Reader reader = new BufferedReader(new InputStreamReader(inputstream, StandardCharsets.UTF_8));
+            JsonElement appendedElement = GsonHelper.fromJson(((JsonReloadListenerAccess) this).getGson(), reader, JsonElement.class);
+            return appendedElement.getAsJsonObject().getAsJsonArray("pools");
+        } catch (IOException e) {
+            Main.LOGGER.error("Could not read appended table:" + location.toString());
+            e.printStackTrace();
+        }
+        return new JsonArray();
     }
 }
