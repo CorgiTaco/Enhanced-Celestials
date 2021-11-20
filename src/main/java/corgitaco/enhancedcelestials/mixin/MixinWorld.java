49b0b0f7d29bb94e90b0326ed37c9c1485884e52
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-11-20 15:11:07 -0800

    Support create and other mods who extend world directly. Closes #77 & Closes #75

---
+++ src/main/java/corgitaco/enhancedcelestials/mixin/MixinWorld.java
@@ -0,0 +1,28 @@
+package corgitaco.enhancedcelestials.mixin;
+
+import corgitaco.enhancedcelestials.EnhancedCelestialsWorldData;
+import corgitaco.enhancedcelestials.LunarContext;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.Nullable;
+
+@Mixin(Level.class)
+public abstract class MixinWorld implements EnhancedCelestialsWorldData {
+
+
+    @Nullable
+    private LunarContext lunarContext;
+
+    @Nullable
+    @Override
+    public LunarContext getLunarContext() {
+        return this.lunarContext;
+    }
+
+    @Override
+    public LunarContext setLunarContext(LunarContext lunarContext) {
+        this.lunarContext = lunarContext;
+        return this.lunarContext;
+    }
+}
--- src/main/java/corgitaco/enhancedcelestials/mixin/client/MixinClientWorld.java
+++ src/main/java/corgitaco/enhancedcelestials/mixin/client/MixinClientWorld.java
@@ -8,28 +8,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
 import java.util.function.BooleanSupplier;
 
 @Mixin(ClientLevel.class)
-public class MixinClientWorld implements EnhancedCelestialsWorldData {
-
-    private LunarContext lunarContext;
-
-    @Nullable
-    @Override
-    public LunarContext getLunarContext() {
-        return this.lunarContext;
-    }
-
-    @Override
-    public LunarContext setLunarContext(LunarContext lunarContext) {
-        this.lunarContext = lunarContext;
-        return this.lunarContext;
-    }
+public abstract class MixinClientWorld {
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void attachLunarTick(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
+        LunarContext lunarContext = ((EnhancedCelestialsWorldData) this).getLunarContext();
         if (lunarContext != null) {
             lunarContext.tick((ClientLevel) (Object) this);
         }
