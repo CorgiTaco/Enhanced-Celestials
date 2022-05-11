463aaa4fe7676a3a3d137c834b77e3f165a2ef1b
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-05-10 23:49:24 -0700

    Use record for lazy codec

--- Common/src/main/java/corgitaco/enhancedcelestials/util/CodecUtil.java
+++ Common/src/main/java/corgitaco/enhancedcelestials/util/CodecUtil.java
@@ -33,12 +33,12 @@ public class CodecUtil {
             return clickEvent.getAction();
         }), Codec.STRING.fieldOf("value").forGetter((clickEvent -> {
             return clickEvent.getValue();
-        })) ).apply(builder, ClickEvent::new);
+        }))).apply(builder, ClickEvent::new);
     });
 
     public static final Codec<Style> STYLE_CODEC = RecordCodecBuilder.create((builder) -> {
         return builder.group(Codec.STRING.optionalFieldOf("color", "").forGetter((style) -> {
-            return style.getColor() != null ? Integer.toHexString(((ColorAccess)(Object) style.getColor()).getColorRaw()) : Integer.toHexString(ChatFormatting.WHITE.getColor());
+            return style.getColor() != null ? Integer.toHexString(((ColorAccess) (Object) style.getColor()).getColorRaw()) : Integer.toHexString(ChatFormatting.WHITE.getColor());
         }), Codec.BOOL.optionalFieldOf("bold", false).forGetter((style) -> {
             return style.isBold();
         }), Codec.BOOL.optionalFieldOf("italic", false).forGetter((style) -> {
@@ -51,17 +51,11 @@ public class CodecUtil {
             return style.isObfuscated();
         }), CLICK_EVENT_CODEC.optionalFieldOf("clickEvent").forGetter((style) -> {
             return style.getClickEvent() != null ? Optional.of(style.getClickEvent()) : Optional.empty();
-        } )).apply(builder, (color, bold, italic, underlined, strikethrough, obfuscated, clickEvent) -> StyleAccess.create(ColorAccess.create(ColorSettings.tryParseColor(color)), bold, italic, underlined, strikethrough, obfuscated, clickEvent.orElse(null), null, null, null));
+        })).apply(builder, (color, bold, italic, underlined, strikethrough, obfuscated, clickEvent) -> StyleAccess.create(ColorAccess.create(ColorSettings.tryParseColor(color)), bold, italic, underlined, strikethrough, obfuscated, clickEvent.orElse(null), null, null, null));
     });
 
 
-
-    public static class LazyCodec<TYPE> implements Codec<TYPE> {
-        private final Supplier<Codec<TYPE>> delegate;
-
-        public LazyCodec(Supplier<Codec<TYPE>> delegate) {
-            this.delegate = delegate;
-        }
+    public record LazyCodec<TYPE>(Supplier<Codec<TYPE>> delegate) implements Codec<TYPE> {
 
         @Override
         public <T> DataResult<T> encode(TYPE input, DynamicOps<T> ops, T prefix) {
@@ -71,10 +65,6 @@ public class CodecUtil {
         @Override
         public <T> DataResult<Pair<TYPE, T>> decode(DynamicOps<T> ops, T input) {
             return this.delegate().get().decode(ops, input);
-        }
-
-        public Supplier<Codec<TYPE>> delegate() {
-            return this.delegate;
         }
     }
 }
