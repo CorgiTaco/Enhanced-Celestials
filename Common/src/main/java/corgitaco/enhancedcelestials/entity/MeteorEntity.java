ac3b3bcbbc4ddee6bee5daeaf1f1d4f978b849c6
⸸ ᴄʜᴀᴏᴛᴛɪᴄ ⸸ <64570529+chaottic@users.noreply.github.com>
2022-09-17 18:18:39 +1000

    meteor entity collision changes

--- Common/src/main/java/corgitaco/enhancedcelestials/entity/MeteorEntity.java
+++ Common/src/main/java/corgitaco/enhancedcelestials/entity/MeteorEntity.java
@@ -1,8 +1,6 @@
 package corgitaco.enhancedcelestials.entity;
 
 import corgitaco.enhancedcelestials.core.ECEntities;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
@@ -11,38 +9,16 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-public class MeteorEntity extends Entity {
-
-
-    public MeteorEntity(EntityType<?> entityType, Level level) {
-        super(entityType, level);
-    }
+public final class MeteorEntity extends Entity {
 
     public MeteorEntity(Level level) {
-        super(ECEntities.METEOR.get(), level);
-
+        this(ECEntities.METEOR.get(), level);
     }
 
-    @Override
-    public void tick() {
-        super.tick();
-        Vec3 velocity = getDeltaMovement();
-        this.move(MoverType.SELF, velocity);
-        this.setDeltaMovement(this.getDeltaMovement().add(0.0D, -0.04D, 0.0D));
-
-        if (onGround && !level.isClientSide) {
-            explode();
-        }
-    }
-
-    @NotNull
-    private void explode() {
-        discard();
-        this.level.explode(this, getX(), getY(), getZ(), 4.0F, Explosion.BlockInteraction.DESTROY);
+    public MeteorEntity(EntityType<? extends MeteorEntity> $$0, Level $$1) {
+        super($$0, $$1);
     }
 
     @Override
@@ -61,22 +37,23 @@ public class MeteorEntity extends Entity {
     }
 
     @Override
-    public Packet<?> getAddEntityPacket() {
+    public @NotNull Packet<?> getAddEntityPacket() {
         return new ClientboundAddEntityPacket(this);
     }
 
     @Override
-    protected void onInsideBlock(BlockState state) {
-        BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
-        for (Direction value : Direction.values()) {
-            BlockState offsetState = level.getBlockState(mutableBlockPos.setWithOffset(this.blockPosition(), value));
-            if (!offsetState.isAir()) {
-                explode();
-            }
+    public void tick() {
+        super.tick();
+
+        var gravity = 0.04D;
+
+        var velocity = getDeltaMovement();
+        move(MoverType.SELF, velocity);
+        setDeltaMovement(velocity.subtract(0.0D, gravity, 0.0D));
+
+        if (!level.isClientSide && onGround || verticalCollision || horizontalCollision) {
+            discard();
+            level.explode(this, getX(), getY(), getZ(), 4.0F, Explosion.BlockInteraction.DESTROY);
         }
-        if (!state.isAir()) {
-            explode();
-        }
-        super.onInsideBlock(state);
     }
 }
