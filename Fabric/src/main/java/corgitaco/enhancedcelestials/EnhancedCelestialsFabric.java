f7110f0328e9ba2ee361f5bada4a8e0fd5c0f9db
Linguardium <61733861+Linguardium@users.noreply.github.com>
2024-04-28 19:18:28 -0500

    bump version to 5.0.0.3 (#199)
    Update loom to 1.5+
    Update fabric loader 0.15.3
    Update fabric api version to 0.92.1
    Update forge to 6.0+
    Update registrationutils to 1.0.1
    Fabric:
    Migrate to FAPI Dynamic registries
    Remove unnecessary mixin
    Add mixin for registry path to match original (remove namespace)

--- Fabric/src/main/java/corgitaco/enhancedcelestials/EnhancedCelestialsFabric.java
+++ Fabric/src/main/java/corgitaco/enhancedcelestials/EnhancedCelestialsFabric.java
@@ -6,20 +6,10 @@ import corgitaco.enhancedcelestials.api.lunarevent.LunarDimensionSettings;
 import corgitaco.enhancedcelestials.api.lunarevent.LunarEvent;
 import corgitaco.enhancedcelestials.core.ECEntities;
 import corgitaco.enhancedcelestials.core.ECRegistries;
-import corgitaco.enhancedcelestials.mixin.RegistryDataLoaderAccess;
-import corgitaco.enhancedcelestials.mixin.RegistrySynchronizationAccess;
 import corgitaco.enhancedcelestials.network.FabricNetworkHandler;
-import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
-import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.core.Registry;
-import net.minecraft.core.RegistrySynchronization;
-import net.minecraft.resources.RegistryDataLoader;
-import net.minecraft.resources.ResourceKey;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EnhancedCelestialsFabric implements ModInitializer {
 
@@ -30,20 +20,8 @@ public class EnhancedCelestialsFabric implements ModInitializer {
         EnhancedCelestials.commonSetup();
         FabricNetworkHandler.init();
 
-
-        List<RegistryDataLoader.RegistryData<?>> registryData = new ArrayList<>(RegistryDataLoader.WORLDGEN_REGISTRIES);
-
-
-        registryData.add(new RegistryDataLoader.RegistryData<>(EnhancedCelestialsRegistry.LUNAR_EVENT_KEY, LunarEvent.DIRECT_CODEC));
-        registryData.add(new RegistryDataLoader.RegistryData<>(EnhancedCelestialsRegistry.LUNAR_DIMENSION_SETTINGS_KEY, LunarDimensionSettings.CODEC));
-
-        RegistryDataLoaderAccess.ec_setWORLDGEN_REGISTRIES(registryData);
-
-        Reference2ObjectMap<ResourceKey<? extends Registry<?>>, RegistrySynchronization.NetworkedRegistryData<?>> map = new Reference2ObjectOpenHashMap<>(RegistrySynchronizationAccess.ec_getNETWORKABLE_REGISTRIES());
-
-        map.put(EnhancedCelestialsRegistry.LUNAR_EVENT_KEY, new RegistrySynchronization.NetworkedRegistryData<>(EnhancedCelestialsRegistry.LUNAR_EVENT_KEY, LunarEvent.DIRECT_CODEC));
-        map.put(EnhancedCelestialsRegistry.LUNAR_DIMENSION_SETTINGS_KEY, new RegistrySynchronization.NetworkedRegistryData<>(EnhancedCelestialsRegistry.LUNAR_DIMENSION_SETTINGS_KEY, LunarDimensionSettings.CODEC));
-        RegistrySynchronizationAccess.ec_setNETWORKABLE_REGISTRIES(map);
+        DynamicRegistries.registerSynced(EnhancedCelestialsRegistry.LUNAR_EVENT_KEY, LunarEvent.DIRECT_CODEC);
+        DynamicRegistries.registerSynced(EnhancedCelestialsRegistry.LUNAR_DIMENSION_SETTINGS_KEY, LunarDimensionSettings.CODEC);
 
         if (EnhancedCelestials.NEW_CONTENT) {
             ECEntities.registerAttributes(FabricDefaultAttributeRegistry::register);
