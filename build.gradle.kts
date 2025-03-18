494128938e1bf746d9027dd8ef0e274abe58ae2d
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2025-03-17 23:42:57 -0700

    Add JT Maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,10 +73,10 @@ subprojects {
         repositories {
             mavenLocal()
             maven {
-                val releasesRepoUrl = "https://example.com/releases"
-                val snapshotsRepoUrl = "https://example.com/snapshots"
+                val releasesRepoUrl = "https://maven.jt-dev.tech/releases"
+                val snapshotsRepoUrl = "https://maven.jt-dev.tech/snapshots"
                 url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
-                name = "ExampleRepo"
+                name = "JTDev-Maven-Repository"
                 credentials {
                     username = project.properties["repoLogin"]?.toString()
                     password = project.properties["repoPassword"]?.toString()
